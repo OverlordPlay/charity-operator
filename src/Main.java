c68c4cd8ca649272119791b9c01fb73e9263bbf6
Learn <egivmir59@gmail.com>
2024-05-06 08:40:32 +0200

    delete else

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,17 @@ public class Main {
 
         int amount = 30_000; // сумма платежа
         int score = 100; // баланс
-
-        int balance = (score + amount); // баланс + платёж
         int bonus = amount / 100; // рассчёт бонусов
-        int total = (balance + bonus); // баланс с бонусами
 
-
-        if (amount >= 1000) {
-            bonus = amount / 100;
-            System.out.println("Итоговый бонус: " + bonus);
-            System.out.println("Баланс: " + total);
-
-        } else (amount < 1000) {
+        if (amount < 1000) {
             bonus = 0;
+        }
+
+        int total = (score + bonus + amount); // баланс с бонусами
+        
             System.out.println("Итоговый бонус: " + bonus);
             System.out.println("Баланс: " + total);
-        }
+
 
     }
 }
