c66cadfb8f1a2752293062385cca89d8fa1e1afc
Learn <egivmir59@gmail.com>
2024-05-07 09:45:11 +0200

    ending

--- src/Main.java
+++ src/Main.java
@@ -10,9 +10,9 @@ public class Main {
         }
 
         int total = (score + bonus + amount); // баланс с бонусами
-        
-            System.out.println("Итоговый бонус: " + bonus);
-            System.out.println("Баланс: " + total);
+
+        System.out.println("Итоговый бонус: " + bonus);
+        System.out.println("Баланс: " + total);
 
 
     }
